cc7416e5e081b55230b9ce39fb9ebc5c41ea0b03
Arya Ludianto <arya.pratamal@ymail.com>
2019-06-12 11:18:11 +0200

    add all function into Vue instance and stateDisplay

---
+++ data/congress-113.js
@@ -0,0 +1,419 @@
+
+
+let stateAbb = [
+  {
+      "name": "Alabama",
+      "abbreviation": "AL"
+  },
+  {
+      "name": "Alaska",
+      "abbreviation": "AK"
+  },
+  {
+      "name": "American Samoa",
+      "abbreviation": "AS"
+  },
+  {
+      "name": "Arizona",
+      "abbreviation": "AZ"
+  },
+  {
+      "name": "Arkansas",
+      "abbreviation": "AR"
+  },
+  {
+      "name": "California",
+      "abbreviation": "CA"
+  },
+  {
+      "name": "Colorado",
+      "abbreviation": "CO"
+  },
+  {
+      "name": "Connecticut",
+      "abbreviation": "CT"
+  },
+  {
+      "name": "Delaware",
+      "abbreviation": "DE"
+  },
+  {
+      "name": "District Of Columbia",
+      "abbreviation": "DC"
+  },
+  {
+      "name": "Federated States Of Micronesia",
+      "abbreviation": "FM"
+  },
+  {
+      "name": "Florida",
+      "abbreviation": "FL"
+  },
+  {
+      "name": "Georgia",
+      "abbreviation": "GA"
+  },
+  {
+      "name": "Guam",
+      "abbreviation": "GU"
+  },
+  {
+      "name": "Hawaii",
+      "abbreviation": "HI"
+  },
+  {
+      "name": "Idaho",
+      "abbreviation": "ID"
+  },
+  {
+      "name": "Illinois",
+      "abbreviation": "IL"
+  },
+  {
+      "name": "Indiana",
+      "abbreviation": "IN"
+  },
+  {
+      "name": "Iowa",
+      "abbreviation": "IA"
+  },
+  {
+      "name": "Kansas",
+      "abbreviation": "KS"
+  },
+  {
+      "name": "Kentucky",
+      "abbreviation": "KY"
+  },
+  {
+      "name": "Louisiana",
+      "abbreviation": "LA"
+  },
+  {
+      "name": "Maine",
+      "abbreviation": "ME"
+  },
+  {
+      "name": "Marshall Islands",
+      "abbreviation": "MH"
+  },
+  {
+      "name": "Maryland",
+      "abbreviation": "MD"
+  },
+  {
+      "name": "Massachusetts",
+      "abbreviation": "MA"
+  },
+  {
+      "name": "Michigan",
+      "abbreviation": "MI"
+  },
+  {
+      "name": "Minnesota",
+      "abbreviation": "MN"
+  },
+  {
+      "name": "Mississippi",
+      "abbreviation": "MS"
+  },
+  {
+      "name": "Missouri",
+      "abbreviation": "MO"
+  },
+  {
+      "name": "Montana",
+      "abbreviation": "MT"
+  },
+  {
+      "name": "Nebraska",
+      "abbreviation": "NE"
+  },
+  {
+      "name": "Nevada",
+      "abbreviation": "NV"
+  },
+  {
+      "name": "New Hampshire",
+      "abbreviation": "NH"
+  },
+  {
+      "name": "New Jersey",
+      "abbreviation": "NJ"
+  },
+  {
+      "name": "New Mexico",
+      "abbreviation": "NM"
+  },
+  {
+      "name": "New York",
+      "abbreviation": "NY"
+  },
+  {
+      "name": "North Carolina",
+      "abbreviation": "NC"
+  },
+  {
+      "name": "North Dakota",
+      "abbreviation": "ND"
+  },
+  {
+      "name": "Northern Mariana Islands",
+      "abbreviation": "MP"
+  },
+  {
+      "name": "Ohio",
+      "abbreviation": "OH"
+  },
+  {
+      "name": "Oklahoma",
+      "abbreviation": "OK"
+  },
+  {
+      "name": "Oregon",
+      "abbreviation": "OR"
+  },
+  {
+      "name": "Palau",
+      "abbreviation": "PW"
+  },
+  {
+      "name": "Pennsylvania",
+      "abbreviation": "PA"
+  },
+  {
+      "name": "Puerto Rico",
+      "abbreviation": "PR"
+  },
+  {
+      "name": "Rhode Island",
+      "abbreviation": "RI"
+  },
+  {
+      "name": "South Carolina",
+      "abbreviation": "SC"
+  },
+  {
+      "name": "South Dakota",
+      "abbreviation": "SD"
+  },
+  {
+      "name": "Tennessee",
+      "abbreviation": "TN"
+  },
+  {
+      "name": "Texas",
+      "abbreviation": "TX"
+  },
+  {
+      "name": "Utah",
+      "abbreviation": "UT"
+  },
+  {
+      "name": "Vermont",
+      "abbreviation": "VT"
+  },
+  {
+      "name": "Virgin Islands",
+      "abbreviation": "VI"
+  },
+  {
+      "name": "Virginia",
+      "abbreviation": "VA"
+  },
+  {
+      "name": "Washington",
+      "abbreviation": "WA"
+  },
+  {
+      "name": "West Virginia",
+      "abbreviation": "WV"
+  },
+  {
+      "name": "Wisconsin",
+      "abbreviation": "WI"
+  },
+  {
+      "name": "Wyoming",
+      "abbreviation": "WY"
+  }
+]
+
+
+
+//URL for API will pass as arguments to fetch function
+const senate = "https://api.propublica.org/congress/v1/113/senate/members.json";
+const house = "https://api.propublica.org/congress/v1/113/house/members.json";
+
+
+
+// -------------------------VUE-INSTANCE------------------------------
+
+var app = new Vue({  
+  el: '#app',  
+  data: {    
+    display:{},
+    members:{},
+    statistics:{
+      "number_of_democrats": 0,
+      "number_of_republicans":0,
+      "number_of_independents":0,
+      "number_of_total":0,
+      "total_vote_with_party":0,
+      "democrats_vote_with_party":0,
+      "republicans_vote_with_party":0,
+      "independents_vote_with_party":0,
+      "members_often_not_vote_with_party":0,
+      "members_often_vote_with_party":0,
+      "members_missed_most_vote":0,
+      "members_missed_least_vote":0
+    } , stateAbb
+  },
+  methods:{
+    fillState : function (members) {
+      let selectedState = document.getElementById("state"),
+      data;
+    
+      if( selectedState.value === "all"){
+        data = members;
+      } else {
+       data = members.filter(data=> {
+       return data.state === selectedState.value
+      });
+    }
+     return this.fillParty(data);
+    },
+    fillParty : function (data) {
+      let rep = document.getElementById("checkRep"),
+        dem = document.getElementById("checkDem"),
+        ind = document.getElementById("checkInd"),
+        dataDisp, tempData=[], tempArr=[rep,dem,ind];
+      
+        if(!rep.checked && !dem.checked && !ind.checked || rep.checked && dem.checked && ind.checked){
+          dataDisp = data;
+        } else if (ind.checked || dem.checked || rep.checked) {
+          tempData = [];
+          data.filter(member => tempArr.forEach(arr => { if(arr.checked){
+          if (member.party === arr.value) tempData.push(member) }}));
+          dataDisp = tempData;
+        } 
+        if (dataDisp.length === 0) dataDisp = [{no_name:"All data are filtered"}]
+       return this.display = dataDisp;
+    },
+    statFill: function (member) {
+        let party = { 
+          democrats: [],
+          republicans: [],
+          independents: [] 
+        };
+      
+        function partFill (data) {
+          data.filter(data=> {
+            if (data.party === "D") party.democrats.push(data)
+            else if (data.party === "R") party.republicans.push(data)
+            else party.independents.push(data)
+          })
+        }
+    
+    
+        function votesWithParty (part){
+          let result = !party[part][0] ? 0 : (party[part].map(dem => dem["votes_with_party_pct"]).reduce((prev, next) => prev + next)/party[part].length)
+          return Math.round(result*100)/100  ;
+        }
+    
+        // function vote --> type = "most"/"least", vote = "party"/"member"
+        function vote (type, vote, data){
+        let time = data.length * 0.1,
+          tempData=[...data],
+            result = [], i=0, 
+            voteType = vote === "party" ? "votes_with_party_pct" : "missed_votes_pct"; 
+
+          tempData.sort ((a,b)=> type === "least" ? a[voteType] - b[voteType] : b[voteType] - a[voteType])
+        
+          while(i <= time ){
+          result.push(tempData[i])
+            while (i>=time && tempData[i][voteType] === tempData[i+1][voteType]) {
+              i++;
+              result.push(tempData[i]);
+              if (tempData[i][voteType]  != tempData[i+1][voteType] ) break;
+            }
+          i++;
+        }
+          return result;
+        }
+
+            partFill(member);
+            this.statistics["number_of_democrats"] = party.democrats.length;
+            this.statistics["number_of_republicans"] = party.republicans.length;
+            this.statistics["number_of_independents"] = party.independents.length;
+            this.statistics["democrats_vote_with_party"] = votesWithParty("democrats");
+            this.statistics["republicans_vote_with_party"] = votesWithParty("republicans");
+            this.statistics["independents_vote_with_party"] = votesWithParty("independents");
+            this.statistics["members_often_not_vote_with_party"] = this.nameVal(vote("least","party",member));
+            this.statistics["members_often_vote_with_party"] = this.nameVal(vote("most","party",member));
+            this.statistics["members_missed_most_vote"] = this.nameVal(vote("most","member",member));
+            this.statistics["members_missed_least_vote"] = this.nameVal(vote("least","member",member));
+            this.statistics["number_of_total"] = Math.round((this.statistics["number_of_democrats"] + this.statistics["number_of_republicans"] + this.statistics["number_of_independents"]) * 100 )/100;
+
+            if(!this.statistics["independents_vote_with_party"]) this.statistics["total_vote_with_party"] = Math.round(((this.statistics["democrats_vote_with_party"] + this.statistics["republicans_vote_with_party"] + this.statistics["independents_vote_with_party"])/2)*100)/100;
+            else this.statistics["total_vote_with_party"] = Math.round(((this.statistics["democrats_vote_with_party"] + this.statistics["republicans_vote_with_party"] + this.statistics["independents_vote_with_party"])/3)*100)/100;
+          },
+           nameVal: function (members) {
+            //name validation---------------------------------
+            let memTemp = []
+            
+            members.forEach(data=> {
+            let first_name = data.first_name || '',
+                middle_name = data.middle_name || '', 
+                last_name= data.last_name || '', 
+                person_full_name = !middle_name && (last_name + " " + first_name) || (last_name + " "+ first_name +" " + middle_name),
+                seniorText = data.seniority == 1 && (data.seniority + " year") || (data.seniority + " years"),
+                percentVoteText = data.votes_with_party_pct + "%";
+                stateAbb.filter(state=> { if(state.abbreviation === data.state) data.state_name = state.name}); 
+          
+              data.full_name = person_full_name;
+              data.seniority_txt = seniorText;
+              data.votes_with_party_pct_txt = percentVoteText; 
+          
+              memTemp.push(data); 
+            })
+            return memTemp;
+          },
+           loader: function () {
+            let loader = document.getElementById("loader"),
+                container = document.getElementById("container");
+          
+                loader.setAttribute("class", "active");
+                container.setAttribute("class", "active")
+          },
+           fetchData: function (dataURL) {
+            let URL = dataURL;
+            fetch(URL, { method:'GET',
+                               headers: { 'X-API-Key' : 'yDopPricaMTxYJvgYSF3d1dah1k2TlgaijneYq1G' }})
+              .then(response => {
+                if(response.ok){ 
+                  return response.json()
+                } else throw new Error(response.statusText)
+              }).then(data => {
+                  this.members = data.results[0].members
+                  this.display = this.nameVal(app.members)
+                  this.statFill(this.members);
+                  this.loader();
+            
+              }).catch(error =>  {
+                console.log('ERROR: ' + error.message)
+              });   
+            
+            },
+            test: function (){
+              console.log("test")
+            }
+          
+
+  }
+}); 
+
+//------------------------------------END-OF-VUE-------------------------------
+
+
+
---
+++ data/statistic.js
@@ -0,0 +1,161 @@
+
+
+var statistics = {
+  "number_of_democrats": 0,
+  "number_of_republicans":0,
+  "number_of_independents":0,
+  "number_of_total":0,
+  "total_vote_with_party":0,
+  "democrats_vote_with_party":0,
+  "republicans_vote_with_party":0,
+  "independents_vote_with_party":0,
+  "members_often_not_vote_with_party":0,
+  "members_often_vote_with_party":0,
+  "members_missed_most_vote":0,
+  "members_missed_least_vote":0
+};
+
+
+
+function statFill (member) {
+  let party = { 
+    democrats: [],
+    republicans: [],
+    independents: [] 
+  };
+
+  function partFill (data) {
+    data.filter(data=> {
+      if (data.party === "D") party.democrats.push(data)
+      else if (data.party === "R") party.republicans.push(data)
+      else party.independents.push(data)
+    })
+}
+
+
+function votesWithParty (part){
+  let result = !party[part][0] ? 0 : (party[part].map(dem => dem["votes_with_party_pct"]).reduce((prev, next) => prev + next)/statistics[`number_of_${part}`])
+  return Math.round(result*100)/100 ;
+}
+
+// function vote --> type = "most"/"least", vote = "party"/"member"
+function vote (type, vote, data){
+let time = data.length * 0.1,
+  tempData=[...data],
+    result = [], i=0, 
+    voteType = vote === "party" ? "votes_with_party_pct" : "missed_votes_pct"; 
+  
+  tempData.sort ((a,b)=> type === "least" ? a[voteType] - b[voteType] : b[voteType] - a[voteType])
+ 
+  while(i <= time ){
+  result.push(tempData[i])
+    while (i>=time && tempData[i][voteType] === tempData[i+1][voteType]) {
+      i++;
+      result.push(tempData[i]);
+      if (tempData[i][voteType]  != tempData[i+1][voteType] ) break;
+    }
+  i++;
+}
+  return result;
+}
+
+    partFill(member);
+    this.statistics["number_of_democrats"] = party.democrats.length;
+    this.statistics["number_of_republicans"] = party.republicans.length;
+    this.statistics["number_of_independents"] = party.independents.length;
+    this.statistics["democrats_vote_with_party"] = votesWithParty("democrats");
+    this.statistics["republicans_vote_with_party"] = votesWithParty("republicans");
+    this.statistics["independents_vote_with_party"] = votesWithParty("independents");
+    this.statistics["members_often_not_vote_with_party"] = vote("least","party",member);
+    this.statistics["members_often_vote_with_party"] = vote("most","party",member);
+    this.statistics["members_missed_most_vote"] = vote ("most","member",member);
+    this.statistics["members_missed_least_vote"] = vote ("least","member",member);
+  }
+
+
+
+
+
+//------------------display the data---------------------
+
+function glanceTab (member) {
+  var tbody = document.getElementById("atGlance");
+  tbody.innerHTML = ""
+  
+      member["number_of_total"] = Math.round((member["number_of_democrats"] + member["number_of_republicans"] + member["number_of_independents"]) * 100 )/100;
+      
+      if(!member["independents_vote_with_party"]) member["total_vote_with_party"] = Math.round(((member["democrats_vote_with_party"] + member["republicans_vote_with_party"] + member["independents_vote_with_party"])/2)*100)/100;
+      else member["total_vote_with_party"] = Math.round(((member["democrats_vote_with_party"] + member["republicans_vote_with_party"] + member["independents_vote_with_party"])/3)*100)/100;
+      
+      let senTab = document.getElementById("atGlance");
+      let textParty = ["Republicans","Democrats", "Independents", "Total"]
+    
+    textParty.forEach(text => {
+      let row = document.createElement("tr");
+      let textNice = [text, member[`number_of_${text.toLowerCase()}`], member[`${text.toLowerCase()}_vote_with_party`]+ " %"]
+        
+      textNice.forEach(textFill => {
+          
+          let col = document.createElement("td");
+          let text = document.createTextNode(textFill)
+          col.appendChild(text)
+          return row.appendChild(col)
+
+        })
+
+        return senTab.appendChild(row)
+
+    })
+}
+
+//---- tab = "leastLoyal" || "mostLoyal" ||  "leastEngagedTab" || "mostEngageTab", member = statistics
+function dispTab (tab , member) {
+  var tbody = document.getElementById(tab);
+  tbody.innerHTML = "";
+
+  let persons;
+ 
+  if (tab === "leastLoyal" || tab === "mostLoyal"){
+    persons = tab === "leastLoyal" ? member.members_often_not_vote_with_party : member.members_often_vote_with_party;  
+  } else if ( tab === "leastEngagedTab" || tab === "mostEngagedTab"){
+     persons = tab === "leastEngagedTab" ? member.members_missed_most_vote : member.members_missed_least_vote;
+  }
+
+  persons.forEach(person=> {
+    let row = document.createElement("tr"), partyVotes, partyVotesPercs;
+  
+      if (tab === "leastLoyal" || tab === "mostLoyal"){
+        partyVotes = document.createTextNode(person.total_votes), 
+        partyVotesPercs = document.createTextNode(person.votes_with_party_pct + " %")
+      } else if ( tab === "leastEngagedTab" || tab === "mostEngagedTab"){
+        partyVotes = document.createTextNode(person.missed_votes), 
+        partyVotesPercs = document.createTextNode(person.missed_votes_pct + " %")
+      }
+
+    let text = [fullName = nameVal(person), partyVotes, partyVotesPercs]
+
+        text.forEach(text=> {
+          let col =document.createElement("td");
+          col.appendChild(text);
+
+          return row.appendChild(col)
+
+        })
+
+        return tbody.appendChild(row);
+  })
+
+}
+
+//----display data statistics---------
+function dispDataLoyalty () {
+  glanceTab(statistics);
+  dispTab ("mostLoyal", statistics)
+  dispTab ("leastLoyal", statistics)
+}
+
+function dispDataAttendance () {
+  glanceTab(statistics);  
+  dispTab("leastEngagedTab", statistics);
+  dispTab("mostEngagedTab", statistics);
+}
